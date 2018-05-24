8a74a78240013ad82906cc38614131b68caeb476
Richard Bisschops <Richard.jh.Bisschops@live.nl>
2018-05-24 15:23:13 +0200

    Some extra documentation for genetic program

--- Genetic code/Genetic programming.py
+++ Genetic code/Genetic programming.py
@@ -99,13 +99,15 @@ class Leaf:
     def getRandomBranch(self):
         return self.clone(), 0
 
+    #Replace a branch in the tree with given tree. If index is zero here, we are the only the branch and thus we need to be replaced.
     def replaceBranch(self,replacer,index):
         index-=index
         if index==0:
             return replacer, index
         else:
-            None, index
+            self, index
 
+    #Clone a selected branch. If not this leaf, send to branch above it was not this one.
     def getBranch(self,selected):
         selected -=1
         if selected==0: #If we happen to have a tree of size 1. Otherwise, replacement would have happened at the branch
@@ -113,12 +115,14 @@ class Leaf:
         else:
             return None, selected
 
+    #Mutate the action with mutationRate chance.
     def mutate(self):
         if random.uniform(0,1)<mutationRate:
             return generateRandomBranch()
         return self
         
-
+# Creates a population of numberOf size. Each member is of the form branch(leaf,leaf).
+# Consideration and actions is randomly selected.
 def createIndividuals(numberOf):
     individuals = []
     for count in range(numberOf):
@@ -131,9 +135,14 @@ def createIndividuals(numberOf):
         individuals.append(newIndividual)
     return individuals
 
+# Genetic programming algorithm. Take a population of individuals, test their fitness, select survivors and create new population out of survivors.
+# Note: if entering population of an uneven individuals, then during the first generation, you will have population+1 individuals. This remains the same for the remainder
+# of the code, regardless of the amount of max generations.
 def evolve(population, maxGenerations):
     for generation in range(maxGenerations):
         print generation
+
+        # Check how good each invididual is in the game.
         fitnesses = []
         game = None #TODO: code for randomly generating a game
         for indi in population:
@@ -142,26 +151,28 @@ def evolve(population, maxGenerations):
                 action=indi.perform(game)
                 usedGame.doAction(action)
             fitnesses.append(usedGame.getScore)
+
+        # Let the individuals compete with each other and keep the winners for the evolution.
         survivors = []
         while not len(fitnesses)==0:
-            if len(fitnesses)==1:
+            if len(fitnesses)==1: # If only one individu left, it wins the competition automatically.
                 survivors.append(population.pop(0)) #This should also remove it from population
                 fitnesses.pop(0)
             else:
                 candidateOne=random.randint(0,len(fitnesses)-1)
                 candidateTwo=random.randint(0,len(fitnesses)-1)
-                while candidateTwo==candidateOne:
-                    candidateTwo=random.randint(0,len(fitnesses))
-                if fitnesses[candidateOne]==fitnesses[candidateTwo]:
+                while candidateTwo==candidateOne: # Keep on trying until the two candidates are different.
+                    candidateTwo=random.randint(0,len(fitnesses)-1)
+                if fitnesses[candidateOne]==fitnesses[candidateTwo]: #Both individuals are equal, pick one at random.
                     if random.randint(0,1)==0:
                         survivors.append(population[candidateOne])
                     else:
                         survivors.append(population[candidateTwo])
-                elif fitnesses[candidateOne]>fitnesses[candidateTwo]:
+                elif fitnesses[candidateOne]>fitnesses[candidateTwo]: #Left is better than right.
                     survivors.append(population[candidateOne])
-                else:
+                else: #Right is better than left.
                     survivors.append(population[candidateTwo])
-                if candidateOne>candidateTwo: #Remove by index: remove largest index first to avoid removing wrong index for it.
+                if candidateOne>candidateTwo: #Remove largest index first to prevent shifting values from removing the wrong index.
                     fitnesses.pop(candidateOne)
                     fitnesses.pop(candidateTwo)
                     population.pop(candidateOne)
@@ -171,8 +182,11 @@ def evolve(population, maxGenerations):
                     fitnesses.pop(candidateOne)
                     population.pop(candidateTwo)
                     population.pop(candidateOne)
+        
+        # Creating the next generation population. Take the survivors and put them in the new population.
+        # Further create children from two parents, and mutate them with random chance.
         while not len(survivors)==0:
-            if len(survivors)==1:
+            if len(survivors)==1: # If uneven amount of survivors, put it in the population and create a child from itself with mutations.
                 population.add(survivors[0])
                 child = survivors.pop(0).clone()
                 child=child.mutate()
@@ -180,7 +194,7 @@ def evolve(population, maxGenerations):
             else:
                 momIndex = random.randint(0,len(survivors)-1)
                 dadIndex = random.randint(0,len(survivors)-1)
-                while adIndex==momIndex:
+                while dadIndex==momIndex: # Keep trying until dad individual is someone else than the mother.
                     dadIndex=random.randint(0,len(survivors)-1)
                 mom=survivors[momIndex]
                 dad=survivors[dadIndex]
@@ -196,6 +210,7 @@ def evolve(population, maxGenerations):
                 daughter=daughter.mutate()
                 population.append(son)
                 population.append(daughter)
+    #Check which individual performs the best.
     fitnesses = []
     game = None #TODO: code for randomly generating a game
     for indi in population:
@@ -212,13 +227,16 @@ def evolve(population, maxGenerations):
             bestIndividu=population[fitnessInd]
     return bestScore, bestIndividu
     
-
+# Generate a random branch: branch can be a leaf or a branch.
+# We pick out a random index from the list [actions,considerations].
+# If index is in actions, create leaf.
+# Else we create a branch with two random actions.
 def generateRandomBranch():
     totalPos=len(actions)+len(considerations)
     chosenIndex=random.randint(0,totalPos-1)
-    if chosenIndex<len(actions):
+    if chosenIndex<len(actions): # Picked a leaf
         return Leaf(actions[chosenIndex])
-    else:
+    else: #Picked a branch
         chosenCons=considerations[len(actions)-chosenIndex]
         ranLeft = random.randint(0,len(actions)-1)
         leftLeaf= Leaf(actions[ranLeft])
@@ -230,4 +248,5 @@ def generateRandomBranch():
 if __name__ == "__main__":
     individuals=10;   
     population=createIndividuals(individuals)
+    score, bestIndividu = evolve(population, 1000)
     
